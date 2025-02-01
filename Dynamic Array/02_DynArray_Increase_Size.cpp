5202eb9008b7b7db87f7e1c777c41c5799da3f48
Shyam Gupta <shyamgupta4004@gmail.com>
2025-02-01 22:09:15 +0530

    Problem No. 2

---
+++ Dynamic Array/02_DynArray_Increase_Size.cpp
@@ -0,0 +1,45 @@
+// In question 1, define a method doubleArray() to increase the size of the array by double of its size.
+
+#include <iostream>
+
+using namespace std;
+
+class DynArray
+{
+private:
+    int capacity;
+    int lastIndex;
+    int *ptr;
+
+public:
+    DynArray(int);
+    void dynArray();
+};
+
+DynArray::DynArray(int size) : capacity(size)
+{
+    if (size <= 0)
+        ptr = NULL;
+    else
+    {
+        lastIndex = -1;
+        ptr = new int[capacity];
+    }
+}
+
+void DynArray::dynArray()
+{
+    int *temp, i;
+    capacity *= 2;
+    temp = new int[capacity];
+    for (i = 0; i <= lastIndex; i++)
+        temp[i] = ptr[i];
+    delete[] ptr;
+    ptr = temp;
+}
+
+int main()
+{
+    DynArray arr(8);
+    return 0;
+}
