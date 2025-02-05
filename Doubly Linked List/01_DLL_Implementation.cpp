09f4ee80c0f4c300479e44e199798c46a4616d89
Shyam Gupta <shyamgupta4004@gmail.com>
2025-02-05 17:26:40 +0530

    Problem No. 1

---
+++ Doubly Linked List/01_DLL_Implementation.cpp
@@ -0,0 +1,29 @@
+/*
+Define a class DLL to implement singly linked list data structure with member
+variable start pointer of type node.
+*/
+
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node() : data(0), next(NULL) {}
+};
+class DLL
+{
+private:
+    node *start;
+
+public:
+    DLL() : start(NULL) {}
+};
+int main()
+{
+    DLL D;
+
+    return 0;
+}
