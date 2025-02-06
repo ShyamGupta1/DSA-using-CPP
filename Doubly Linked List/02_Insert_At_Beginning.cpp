9d8a960e03c91c3b43704535d50d972e766cffc5
Shyam Gupta <shyamgupta4004@gmail.com>
2025-02-06 23:04:27 +0530

    Problem No. 2

---
+++ Doubly Linked List/02_Insert_At_Beginning.cpp
@@ -0,0 +1,38 @@
+/*
+In question 1, define a method to insert a data into the list at the beginning.
+*/
+
+#include <iostream>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *next;
+    node *prev;
+    node() : data(0), next(NULL), prev(NULL) {}
+};
+class DLL
+{
+private:
+    node *start;
+
+public:
+    DLL() : start(NULL) {}
+    void insertAtStart(int);
+};
+
+void DLL::insertAtStart(int data)
+{
+    node *newNode = new node;
+    newNode->data = data;
+    newNode->next = start;
+    newNode->prev = NULL;
+    start = newNode;
+}
+int main()
+{
+    DLL D;
+
+    return 0;
+}
