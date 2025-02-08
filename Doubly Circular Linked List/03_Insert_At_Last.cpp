9710d6740641f53006e1928ab6dc139ee62eba96
Shyam Gupta <shyamgupta4004@gmail.com>
2025-02-09 00:27:01 +0530

    Problem No. 3

---
+++ Doubly Circular Linked List/03_Insert_At_Last.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    node *prev;
+    int data;
+    node *next;
+
+public:
+    node() : prev(NULL), data(0), next(NULL) {}
+};
+
+class DCLL
+{
+private:
+    node *start;
+
+public:
+    DCLL() : start(NULL) {}
+    void insertAtStart(int);
+    void insertAtLast(int);
+};
+
+void DCLL::insertAtLast(int data)
+{
+    node *newNode = new node;
+    newNode->data = data;
+
+    if (start == NULL)
+    {
+        newNode->next = newNode;
+        newNode->prev = newNode;
+        start = newNode;
+    }
+    else
+    {
+
+        newNode->next=start;
+        newNode->prev=start->prev;
+        start->prev->next=newNode;
+        start->prev=newNode;
+        start=newNode; 
+
+    }
+}
+void DCLL::insertAtStart(int data)
+{
+    node *newNode = new node;
+    newNode->data = data;
+
+    if (start == NULL)
+    {
+        newNode->next = newNode;
+        newNode->prev = newNode;
+    }
+    else
+    {
+        newNode->next = start;
+        newNode->prev = start->prev;
+        start->prev->next = newNode;
+        start->prev = newNode;
+    }
+    start = newNode;
+};
+
+int main()
+{
+    DCLL DC;
+
+    return 0;
+}
