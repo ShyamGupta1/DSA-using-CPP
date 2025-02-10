6f3f2b0d733000f68bf67d908282a70ba141ef44
Shyam Gupta <shyamgupta4004@gmail.com>
2025-02-10 20:42:31 +0530

    Problem No. 6

---
+++ Stack Using Arrays/06_Destructor.cpp
@@ -0,0 +1,78 @@
+/*In question 1, define a destructor to deallocates the memory.*/
+
+#include <iostream>
+using namespace std;
+
+class Stack
+{
+private:
+    int capacity;
+    int top;
+    int *ptr;
+
+public:
+    Stack(int capacity) : capacity(capacity), top(-1)
+    {
+        ptr = new int[this->capacity];
+    }
+    void push(int);
+    bool isFull();
+    bool isEmpty();
+    int peek();
+    int pop();
+    ~Stack()
+    {
+        delete[] ptr;
+    }
+};
+
+int Stack::pop()
+{
+    try
+    {
+        if (top == -1)
+            throw 1;
+        return ptr[--top];
+    }
+    catch (int e)
+    {
+        cout << "\nEmpty Stack";
+    }
+}
+int Stack::peek()
+{
+    try
+    {
+        if (top == -1)
+            throw 1;
+        return ptr[top];
+    }
+    catch (int e)
+    {
+        cout << "\nEmpty Underflow";
+    }
+}
+
+void Stack::push(int data)
+{
+    if (isFull())
+        cout << "\nStack Overflow";
+    else
+        ptr[++top] = data;
+}
+
+bool Stack::isFull()
+{
+    return top + 1 == capacity;
+}
+
+bool Stack::isEmpty()
+{
+    return top == -1;
+}
+
+int main()
+{
+
+    return 0;
+}
