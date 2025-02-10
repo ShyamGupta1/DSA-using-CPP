2150419cc4d7b3feb021a0b726195b8afe98f203
Shyam Gupta <shyamgupta4004@gmail.com>
2025-02-10 20:40:07 +0530

    Problem No. 1

---
+++ Stack Using Arrays/01_Stack_Implementation.cpp
@@ -0,0 +1,17 @@
+// Define a class Stack with capacity, top and ptr pointer as member variables. Implement stack using array.
+
+#include <iostream>
+using namespace std;
+
+class Stack
+{
+private:
+    int capacity;
+    int top;
+    int *ptr;
+};
+
+int main()
+{
+    return 0;
+}
