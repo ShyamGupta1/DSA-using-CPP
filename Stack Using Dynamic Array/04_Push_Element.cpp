5f0049901f40d6b7bc09e499b423f9aa3d77505c
Shyam Gupta <shyamgupta4004@gmail.com>
2025-02-12 13:33:31 +0530

    Problem No. 4

---
+++ Stack Using Dynamic Array/04_Push_Element.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+class Stack
+{
+private:
+    int capacity;
+    int top;
+    int *start;
+
+protected:
+    void doubleStack();
+    void halfStack();
+
+public:
+    Stack() : start(NULL) {}
+    Stack(int capacity) : capacity(capacity), top(-1)
+    {
+        start = new int[capacity];
+    }
+    bool isFull();
+    bool isEmpty();
+    void push(int);
+};
+
+void Stack::push(int data)
+{
+    if (isFull())
+        doubleStack();
+    start[++top] = data;
+}
+
+bool Stack::isEmpty()
+{
+    return top == -1;
+}
+
+bool Stack::isFull()
+{
+    return top == capacity - 1;
+}
+
+void Stack::doubleStack()
+{
+    capacity *= 2;
+    int *ptr = new int[capacity];
+    for (int i = 0; i <= top; i++)
+        ptr[i] = start[i];
+    delete[] start;
+    start = ptr;
+}
+
+void Stack::halfStack()
+{
+    capacity /= 2;
+    int *ptr = new int[capacity];
+    for (int i = 0; i <= top; i++)
+        ptr[i] = start[i];
+    delete[] start;
+    start = ptr;
+}
+
+int main()
+{
+    return 0;
+}
