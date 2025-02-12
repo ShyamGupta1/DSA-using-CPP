62e92a723b6cc37756cc62fc1de20c6d3663961f
Shyam Gupta <shyamgupta4004@gmail.com>
2025-02-12 13:34:00 +0530

    Problem No. 7

---
+++ Stack Using Dynamic Array/07_Destructor.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+using namespace std;
+
+class Stack
+{
+private:
+    int capacity;
+    int top;
+    int *start;
+
+protected:
+    void doubleStack();
+    void halfStack();
+
+public:
+    Stack() : start(NULL) {}
+    Stack(int capacity) : capacity(capacity), top(-1)
+    {
+        start = new int[capacity];
+    }
+    bool isFull();
+    bool isEmpty();
+    void push(int);
+    int peek();
+    int pop();
+    ~Stack() { delete[] start; }
+};
+
+int Stack::pop()
+{
+    if (isEmpty())
+        throw new exception();
+    int temp = start[top];
+    top--;
+    if ((top + 1) * 2 <= capacity && top > 1)
+        halfStack();
+    return temp;
+}
+int Stack::peek()
+{
+    if (isEmpty())
+        throw new exception();
+    return start[top];
+}
+
+void Stack::push(int data)
+{
+    if (isFull())
+        doubleStack();
+    start[++top] = data;
+}
+
+bool Stack::isEmpty()
+{
+    return top == -1;
+}
+
+bool Stack::isFull()
+{
+    return top == capacity - 1;
+}
+
+void Stack::doubleStack()
+{
+    capacity *= 2;
+    int *ptr = new int[capacity];
+    for (int i = 0; i <= top; i++)
+        ptr[i] = start[i];
+    delete[] start;
+    start = ptr;
+}
+
+void Stack::halfStack()
+{
+    capacity /= 2;
+    int *ptr = new int[capacity];
+    for (int i = 0; i <= top; i++)
+        ptr[i] = start[i];
+    delete[] start;
+    start = ptr;
+}
+
+int main()
+{
+    return 0;
+}
