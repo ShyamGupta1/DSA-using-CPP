396cd23f0e67cf57af42363aeaa9bf47af2a1bf9
Shyam Gupta <shyamgupta4004@gmail.com>
2025-02-13 22:24:10 +0530

    Problem No. 5

---
+++ Stack Using Linked List/05_Destructor.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node() : data(0), next(NULL) {}
+};
+
+class Stack
+{
+private:
+    node *top;
+
+public:
+    Stack() : top(NULL) {}
+    void push(int);
+    int peek();
+    int pop();
+    ~Stack();
+};
+
+Stack::~Stack()
+{
+    while (top)
+        pop();
+}
+
+int Stack::pop()
+{
+    if (top == NULL)
+        throw 1;
+    node *temp = top;
+    int data = temp->data;
+    top = temp->next;
+    delete temp;
+    return data;
+}
+
+int Stack::peek()
+{
+    if (top == NULL)
+        throw 1;
+    return top->data;
+}
+void Stack::push(int data)
+{
+    node *newNode = new node;
+    newNode->data = data;
+    newNode->next = top;
+    top = newNode;
+}
+
+int main()
+{
+    Stack S;
+
+    return 0;
+}
