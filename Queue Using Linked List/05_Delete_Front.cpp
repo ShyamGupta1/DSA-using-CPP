6483f8f0ab2e1968879390613e30e78c64e0b594
Shyam Gupta <shyamgupta4004@gmail.com>
2025-02-18 18:51:49 +0530

    Problem No. 5

---
+++ Queue Using Linked List/05_Delete_Front.cpp
@@ -0,0 +1,80 @@
+// In question 1, define a method to delete the front element of the queue.
+
+// In question 1, define a method to view front element in the queue.
+
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+};
+class Queue
+{
+private:
+    node *front, *rear;
+
+public:
+    Queue() : front(NULL), rear(NULL) {}
+    void insert(int);
+    int getRear();
+    int getFront();
+    void deleteFront();
+};
+
+void Queue::deleteFront()
+{
+    if (front)
+    {
+        if (front == rear)
+        {
+            delete front;
+            front = rear = NULL;
+        }
+        else
+        {
+            node *temp = front;
+            front = front->next;
+            delete temp;
+        }
+    }
+}
+
+int Queue::getRear()
+{
+    if (front == NULL)
+        throw 1;
+    else
+        return front->data;
+}
+
+int Queue::getRear()
+{
+    if (rear == NULL)
+        throw 1;
+    else
+        return rear->data;
+}
+
+void Queue::insert(int data)
+{
+    node *newNode = new node;
+    newNode->next = NULL;
+    newNode->data = data;
+    if (front == NULL)
+    {
+        front = newNode;
+        rear = newNode;
+    }
+    else
+    {
+        rear->next = newNode;
+        rear = newNode;
+    }
+}
+int main()
+{
+    return 0;
+}
