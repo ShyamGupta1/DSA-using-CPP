e700bcff09b8c51e10e3edad6c20bfb9fa7864da
Shyam Gupta <shyamgupta4004@gmail.com>
2025-02-18 18:46:34 +0530

    Problem No. 3

---
+++ Queue Using Linked List/03_Get_Rear.cpp
@@ -0,0 +1,50 @@
+// In question 1, define a method to view rear element in the queue.
+
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+};
+class Queue
+{
+private:
+    node *front, *rear;
+
+public:
+    Queue() : front(NULL), rear(NULL) {}
+    void insert(int);
+    int getRear();
+};
+
+int Queue::getRear()
+{
+    if (rear == NULL)
+        throw 1;
+    else
+        return rear->data;
+}
+
+void Queue::insert(int data)
+{
+    node *newNode = new node;
+    newNode->next = NULL;
+    newNode->data = data;
+    if (front == NULL)
+    {
+        front = newNode;
+        rear = newNode;
+    }
+    else
+    {
+        rear->next = newNode;
+        rear = newNode;
+    }
+}
+int main()
+{
+    return 0;
+}
