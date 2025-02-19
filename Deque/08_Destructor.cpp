b001de5a518b2921ac37d167d1aa58d9e19c11d0
Shyam Gupta <shyamgupta4004@gmail.com>
2025-02-19 15:52:47 +0530

    Problem No. 8

---
+++ Deque/08_Destructor.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *prev;
+    node *next;
+};
+class Deque
+{
+private:
+    node *front;
+    node *rear;
+
+public:
+    Deque() : front(NULL), rear(NULL) {}
+    void insertFront(int);
+    void insertRear(int);
+    void deleteFront();
+    void deleteRear();
+    int getFront();
+    int getRear();
+    ~Deque()
+    {
+        while (front)
+            deleteFront();
+    }
+};
+
+int Deque::getRear()
+{
+    if (rear == NULL)
+        throw 1;
+    return rear->data;
+}
+
+int Deque::getFront()
+{
+    if (front == NULL)
+        throw 1;
+    return front->data;
+}
+void Deque::deleteRear()
+{
+    if (rear)
+    {
+        // Single Node
+        if (front == rear) // or rear->prev == NULL
+        {
+            delete front;
+            front = rear = NULL;
+        }
+        else
+        {
+            node *temp = rear;
+            rear = rear->prev;
+            rear->next = NULL;
+            delete temp;
+        }
+    }
+}
+
+void Deque::deleteFront()
+{
+    if (front)
+    {
+        // Single Node
+        if (front == rear) // or front->next == NULL
+        {
+            delete front;
+            front = rear = NULL;
+        }
+        else
+        {
+            node *temp = front;
+            front = front->next;
+            front->prev = NULL;
+            delete temp;
+        }
+    }
+}
+
+void Deque::insertRear(int data)
+{
+    node *newNode = new node;
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if (rear == NULL)
+    {
+        // Deque is Empty
+        newNode->prev = NULL;
+        front = rear = newNode;
+    }
+    else
+    {
+        newNode->prev = rear;
+        rear->next = newNode;
+        rear = newNode;
+    }
+}
+
+void Deque::insertFront(int data)
+{
+    node *newNode = new node;
+    newNode->data = data;
+    newNode->prev = NULL;
+
+    if (front == NULL)
+    {
+        // Deque is Empty
+        newNode->next = NULL;
+        front = rear = newNode;
+    }
+    else
+    {
+        newNode->next = front;
+        front->prev = newNode;
+        front = newNode;
+    }
+}
+
+int main()
+{
+    return 0;
+}
