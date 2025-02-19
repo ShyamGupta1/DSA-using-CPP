65f9eed91638d34ed1586967be25ab0aee84bf98
Shyam Gupta <shyamgupta4004@gmail.com>
2025-02-19 15:52:01 +0530

    Problem No. 3

---
+++ Deque/03_Insert_At_Rear.cpp
@@ -0,0 +1,68 @@
+// In question 1, define a method to insert a new element at the rear.
+
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *prev;
+    node *next;
+};
+class Deque
+{
+private:
+    node *front;
+    node *rear;
+
+public:
+    Deque() : front(NULL), rear(NULL) {}
+    void insertFront(int);
+    void insertRear(int);
+};
+
+void Deque::insertRear(int data)
+{
+    node *newNode = new node;
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if (rear == NULL)
+    {
+        // Deque is Empty
+        newNode->prev = NULL;
+        front = rear = newNode;
+    }
+    else
+    {
+        newNode->prev = rear;
+        rear->next = newNode;
+        rear = newNode;
+    }
+}
+
+void Deque::insertFront(int data)
+{
+    node *newNode = new node;
+    newNode->data = data;
+    newNode->prev = NULL;
+
+    if (front == NULL)
+    {
+        // Deque is Empty
+        newNode->next = NULL;
+        front = rear = newNode;
+    }
+    else
+    {
+        newNode->next = front;
+        front->prev = newNode;
+        front = newNode;
+    }
+}
+
+int main()
+{
+    return 0;
+}
