ca85e803f4e1dc124cde3357f96286a99828be26
Shyam Gupta <shyamgupta4004@gmail.com>
2025-02-19 15:52:08 +0530

    Problem No. 4

---
+++ Deque/04_Delete_Front.cpp
@@ -0,0 +1,89 @@
+// In question 1, define a method to delete front element.
+
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *prev;
+    node *next;
+};
+class Deque
+{
+private:
+    node *front;
+    node *rear;
+
+public:
+    Deque() : front(NULL), rear(NULL) {}
+    void insertFront(int);
+    void insertRear(int);
+    void deleteFront();
+};
+
+void Deque::deleteFront()
+{
+    if (front == NULL)
+        cout << "\nDeque is Empty";
+    // Single Node
+    if(front==rear) // or front->next == NULL
+    {
+        delete front;
+        front=rear=NULL;
+    }
+    else
+    {
+        node* temp=front;
+        front=front->next;
+        front->prev=NULL;
+        delete temp;
+
+    }
+}
+
+void Deque::insertRear(int data)
+{
+    node *newNode = new node;
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if (rear == NULL)
+    {
+        // Deque is Empty
+        newNode->prev = NULL;
+        front = rear = newNode;
+    }
+    else
+    {
+        newNode->prev = rear;
+        rear->next = newNode;
+        rear = newNode;
+    }
+}
+
+void Deque::insertFront(int data)
+{
+    node *newNode = new node;
+    newNode->data = data;
+    newNode->prev = NULL;
+
+    if (front == NULL)
+    {
+        // Deque is Empty
+        newNode->next = NULL;
+        front = rear = newNode;
+    }
+    else
+    {
+        newNode->next = front;
+        front->prev = newNode;
+        front = newNode;
+    }
+}
+
+int main()
+{
+    return 0;
+}
