35ef5934cc3a6fac0b847da5dfd90bca8edf7f76
Shyam Gupta <shyamgupta4004@gmail.com>
2025-02-20 20:10:22 +0530

    Problem No. 1

---
+++ Tree/01_Tree_Implementation.cpp
@@ -0,0 +1,27 @@
+/*
+Define a class BST (Binary Search Tree) with node type pointer root as member
+variable. Implement Binary Search Tree using linked representation.
+*/
+
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    node *left;
+    int data;
+    node *right;
+};
+
+class BST
+{
+private:
+    node *root;
+};
+
+int main()
+{
+
+    return 0;
+}
