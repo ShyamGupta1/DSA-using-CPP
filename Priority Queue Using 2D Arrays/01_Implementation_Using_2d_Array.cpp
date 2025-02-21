6a2d2300b394ea4beedc2a24d053e6dad15a0ed1
Shyam Gupta <shyamgupta4004@gmail.com>
2025-02-21 17:01:27 +0530

    Implementation

---
+++ Priority Queue Using 2D Arrays/01_Implementation_Using_2d_Array.cpp
@@ -0,0 +1,19 @@
+// Define a logic to implement priority queue using 2 dimensional arrays.
+
+#include <iostream>
+using namespace std;
+
+class PriorityQueue
+{
+private:
+    int capacity;         // Size of Each Array (Queue)
+    int highest_priority; // Highest Priority number
+    int **ptr;            // 2D array to store each array (Queue)
+    int *front;           // Array to store front index of each array (Queue)
+    int *rear;            // Array to store rear index of each array (Queue)
+};
+
+int main()
+{
+    return 0;
+}
