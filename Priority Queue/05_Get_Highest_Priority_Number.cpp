2b2e14adc417a1e504f93d1e1693f0f7347f4b9c
Shyam Gupta <shyamgupta4004@gmail.com>
2025-02-21 16:20:41 +0530

    Problem No. 5

---
+++ Priority Queue/05_Get_Highest_Priority_Number.cpp
@@ -0,0 +1,87 @@
+// In question 1, define a method to get highest priority number.
+
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+    int pno; // Priority Number
+};
+class PriorityQueue
+{
+private:
+    node *start;
+
+public:
+    PriorityQueue() : start(NULL) {}
+    void insert(int, int);
+    void deleteNode();
+    int getHighestPriorityElement();
+    int getHighestPriorityNumber();
+};
+
+int PriorityQueue::getHighestPriorityNumber()
+{
+    if (start == NULL)
+        throw 1;
+    return start->pno;
+}
+
+int PriorityQueue::getHighestPriorityElement()
+{
+    if (start == NULL)
+        throw 1;
+    return start->data;
+}
+
+void PriorityQueue::deleteNode()
+{
+    if (start)
+    {
+        node *temp = start;
+        start = start->next;
+        delete temp;
+    }
+}
+
+void PriorityQueue::insert(int data, int pno)
+{
+    node *newNode = new node;
+    newNode->data = data;
+    newNode->pno = pno;
+    if (start == NULL)
+    {
+        start = newNode;
+        newNode->next = NULL;
+    }
+    else
+    {
+        node *temp = start;
+        node *prev = NULL;
+        while (temp)
+        {
+            if (pno > temp->pno)
+                break;
+            prev = temp;
+            temp = temp->next;
+        }
+        if (prev)
+        {
+            newNode->next = temp;
+            prev->next = newNode;
+        }
+        else
+        {
+            newNode->next = temp;
+            start = newNode;
+        }
+    }
+}
+
+int main()
+{
+    return 0;
+}
