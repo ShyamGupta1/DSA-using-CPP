d46ead72e174df350005e587995adddf0efa138b
Shyam Gupta <shyamgupta4004@gmail.com>
2025-02-21 20:55:17 +0530

    Problem No. 6

--- Queue Using Circular Arrays/06_Get_Front.cpp
+++ Queue Using Circular Arrays/06_Get_Front.cpp
@@ -67,7 +67,7 @@ void Queue::insertRear(int data)
         ptr[rear] = data;
     }
 
-    else if (rear != capacity - 1)
+    else if (rear == capacity - 1)
     {
         rear = 0;
         ptr[rear] = data;
