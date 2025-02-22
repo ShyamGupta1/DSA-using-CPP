13b88d33b014f58452acf515bd1f6bef9490db2e
Shyam Gupta <shyamgupta4004@gmail.com>
2025-02-22 22:09:40 +0530

    Problem No. 1

---
+++ Graph/01_Graph_Matrix_Implementation.cpp
@@ -0,0 +1,19 @@
+/*
+Define a class Graph using matrix representation with v_count, e _ count and adj
+pointer as instance members.
+*/
+
+#include <iostream>
+using namespace std;
+
+class GraphMatrix
+{
+private:
+    int v_count;
+    int e_count;
+    int **adj;
+};
+int main()
+{
+    return 0;
+}
